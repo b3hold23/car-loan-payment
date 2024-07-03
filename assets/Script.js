1529b84d431df74dc876f289b578f3173d9c4b9d
unknown <rosa.angel.daniel@gmail.com>
2024-07-02 22:31:54 -0600

    added modal to page

--- assets/Script.js
+++ assets/Script.js
@@ -1,6 +1,40 @@
 // Pseudocode
 
 // 1. Collect up to three third-party quotes (optional)
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("modalBtn");
+    var span = document.getElementById("window-close")[0];
+
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    function closeModal() {
+        modal.style.display = "none"
+    }
+
+    if (btn) {
+        btn.addEventListener("click", openModal);
+    }
+
+    if (span) {
+        span.addEventListener("click", openModal);
+    }
+
+    if (span) {
+        span.addEventListener("click", closeModal);
+    }
+    
+
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            closeModal();
+        }
+    })
+})
+
+
 
 // 2. Input MSRP, Down Payment, and Loan Term
 const MSRP = document.getElementById("");
