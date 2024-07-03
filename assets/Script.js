a20ace6a68ab411a6a8221ecd12c4991acc96f4f
Rory Dowse <rorydowse@hotmail.com>
2024-07-02 21:47:46 -0700

    update JS base monthly cost

--- assets/Script.js
+++ assets/Script.js
@@ -3,7 +3,32 @@
 // 1. Collect up to three third-party quotes (optional)
 
 // 2. Input MSRP, Down Payment, and Loan Term
-const MSRP = document.getElementById("");
+let MSRP;
+let downPayment;
+let loanTerm;
+let baseCost;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button[type='submit'");
+
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    MSRP = parseFloat(document.getElementById("car-price").value);
+    downPayment = parseFloat(document.getElementById("down-payment").value);
+    loanTerm = parseFloat(document.getElementById("loan-term").value);
+
+    console.log(`MSRP: $${MSRP}`);
+    console.log(`Down Payment: $${downPayment}`);
+    console.log(`Loan Term: ${loanTerm}`);
+
+    baseCost = (MSRP - downPayment) / loanTerm;
+    console.log(`Base Monthly Cost: $${baseCost.toFixed(2)}`);
+
+    let monthlyPayment = document.getElementById("monthly-payment");
+    monthlyPayment.textContent = `$${baseCost.toFixed(2)}`;
+  });
+});
 // 3. View base monthly cost
 
 // 4. View difference over loan term between base monthly cost and highest quote
