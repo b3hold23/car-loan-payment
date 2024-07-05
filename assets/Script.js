1154e2e2b464aa357d39e1b01680419a01ef7656
Rory Dowse <rorydowse@hotmail.com>
2024-07-05 10:23:02 -0700

    get inputs from local storage

--- assets/Script.js
+++ assets/Script.js
@@ -9,3 +9,9 @@ function submitInputs() {
   let loanTerm = document.getElementById("loan-term").value;
   localStorage.setItem("loan-term", loanTerm);
 }
+
+// get inputs from local storage
+let savedVehiclePrice = this.localStorage.getItem("vehicle-price");
+let savedDownPayment = this.localStorage.getItem("down-payment");
+let savedLoanTerm = this.localStorage.getItem("loan-term");
+let monthlyPayment = (savedVehiclePrice - savedDownPayment) / savedLoanTerm;
