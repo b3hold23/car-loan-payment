1f9809e8aabaa3dafbbdcdd98e72afd01d80b652
Rory Dowse <rorydowse@hotmail.com>
2024-07-05 11:06:41 -0700

    add basic styling

--- assets/Script.js
+++ assets/Script.js
@@ -151,6 +151,7 @@ window.addEventListener("load", function () {
     );
     let tipButton = this.document.createElement("button");
     tipButton.setAttribute("id", "tips-button");
+    tipButton.setAttribute("class", "btn btn-primary");
     tipButton.textContent = `Tips`;
     tipButtonContainer.appendChild(tipButton);
 
